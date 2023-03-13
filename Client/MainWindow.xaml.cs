d134e352876cf8cf25ab3ac2e26171cef8978570
Leyla Shafiyeva <llshafi03@gmail.com>
2023-03-13 20:43:54 +0400

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Drawing;
+
+namespace Client;
+
+public partial class MainWindow : Window
+{
+    private Socket client;
+    private EndPoint remoteEP;
+    public MainWindow()
+    {
+        InitializeComponent();
+        client = new Socket(AddressFamily.InterNetwork, 
+                            SocketType.Dgram, 
+                            ProtocolType.Udp);
+
+
+        var ip = IPAddress.Parse("127.0.0.1");
+        var port = 45678;
+        remoteEP = new IPEndPoint(ip, port);
+    }
+
+    private async void btnRequest_Click(object sender, RoutedEventArgs e)
+    {
+        var buffer = new byte[ushort.MaxValue - 29];
+        await client.SendToAsync(buffer, SocketFlags.None, remoteEP);
+        var list = new List<byte>();
+        var len = 0;
+        do
+        {
+            var result = await client.ReceiveFromAsync(buffer, SocketFlags.None, remoteEP);
+            len = result.ReceivedBytes;
+
+            list.AddRange(buffer.Take(len));
+
+            try
+            {
+                var image = LoadImage(list.ToArray());
+                if (image != null)
+                    Image.Source = image;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }while (len == buffer.Length);
+
+    }
+    private static BitmapImage? LoadImage(byte[] imageData)
+    {
+        var image = new BitmapImage();
+        image.BeginInit();
+        image.StreamSource = new MemoryStream(imageData);
+        image.CacheOption = BitmapCacheOption.OnLoad;
+        image.EndInit();
+        return image;
+    }
+
+}
---
+++ Server/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+var listener = new Socket(AddressFamily.InterNetwork,
+    SocketType.Dgram,
+    ProtocolType.Udp);
+
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 45678;
+
+var buffer = new byte[ushort.MaxValue - 29];
+var ep = new IPEndPoint(ip, port);
+listener.Bind(ep);
+EndPoint remoteEp = new IPEndPoint(IPAddress.Any, 0);
+
+
+while (true)
+{
+    var result = await listener.ReceiveFromAsync(buffer, SocketFlags.None, remoteEp);
+    var img = TakeScreenShot();
+    var imgBuffer = ImageToByte(img);
+    Console.WriteLine(imgBuffer.Length);
+    var chunk = imgBuffer.Chunk(ushort.MaxValue - 29);
+    var newBuffer = chunk.ToArray();
+
+    for (int i = 0; i < newBuffer.Length; i++)
+    {
+        await Task.Delay(50);
+        await listener.SendToAsync(newBuffer[i], SocketFlags.None, result.RemoteEndPoint);
+    }
+}
+
+
+byte[] ImageToByte(Image img)
+{
+    using (MemoryStream ms = new MemoryStream())
+    {
+        img.Save(ms, ImageFormat.Png);
+        byte[] imageBytes = ms.ToArray();
+        return imageBytes;
+    }
+}
+
+Image TakeScreenShot()
+{
+    Rectangle bounds = Screen.PrimaryScreen.Bounds;
+
+    Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height);
+
+    using (Graphics graphics = Graphics.FromImage(bitmap))
+    {
+        graphics.CopyFromScreen(bounds.X, bounds.Y, 0, 0, bounds.Size);
+    }
+
+    return bitmap;
+}
